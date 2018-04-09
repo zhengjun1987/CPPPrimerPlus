876c9ad1ef574009ffe475f6a214717d43e53e84
郑军 <zhengjun1987@outlook.com>
2018-04-09 23:57:27 +0800

    chapter02 start

---
+++ src/chapter02/myfirst.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+/**
+ *  Author:Zheng Jun
+ *  Date:2018/4/9 23:51
+ *  E-mail:zhengjun1987@outlook.com
+ *  Desc:
+ */
+
+int main(){
+    using namespace std;
+    cout << "Come up and C++ me some time!";
+    cout << endl;
+    cout << "You won'y regret it!" << endl;
+    return 0;
+}
+//Come up and C++ me some time!
+//You won'y regret it!
+//
+//Process finished with exit code 0
